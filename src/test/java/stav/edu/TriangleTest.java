6714a8b86a2c3adf452add27af600607b899cb9d
yettimon <globalobjectiveart@gmail.com>
2020-08-09 11:36:47 +0200

    adding more tests

--- src/test/java/stav/edu/TriangleTest.java
+++ src/test/java/stav/edu/TriangleTest.java
@@ -6,13 +6,21 @@ class TriangleTest {
     Triangle triangle = new Triangle(3,4,5,6);
 
     @org.junit.jupiter.api.Test
-    void getArea() {
+    void getAreaEqualsTrue() {
         Assert.assertEquals(9,triangle.getArea(), 0.01);
+    }
+    void getAreaEqualsFalse() {
+        Assert.assertEquals(33,triangle.getArea(), 0.01);
     }
 
     @org.junit.jupiter.api.Test
-    void getPerimeter() {
+    void getPerimeterEqualsTrue() {
         Assert.assertEquals(12,triangle.getPerimeter(),0.01);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPerimeterEqualsFalse() {
+        Assert.assertEquals(21,triangle.getPerimeter(),0.01);
     }
 
 }
